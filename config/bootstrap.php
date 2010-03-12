5d891ca52476e2976f12ff3054e5a0e8756939bd
Rodrigo Moyle <rodrigorm@gmail.com>
2010-03-12 12:01:05 -0300

    Ajuste no bootstrap para não sobreescrever os inflections definido no arquivo APP/config/inflections.php

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -21,11 +21,11 @@ Configure::write('localePaths', $localePaths);
 // Alteração do inflection
 require dirname(__FILE__) . DS . 'inflections.php';
 $inflector = Inflector::getInstance();
-$inflector->__pluralRules = $pluralRules;
-$inflector->__uninflectedPlural = $uninflectedPlural;
-$inflector->__irregularPlural = $irregularPlural;
-$inflector->__singularRules = $singularRules;
-$inflector->__uninflectedSingular = $uninflectedPlural;
-$inflector->__irregularSingular = array_flip($irregularPlural);
+$inflector->__pluralRules += $pluralRules;
+$inflector->__uninflectedPlural += $uninflectedPlural;
+$inflector->__irregularPlural += $irregularPlural;
+$inflector->__singularRules += $singularRules;
+$inflector->__uninflectedSingular += $uninflectedPlural;
+$inflector->__irregularSingular += array_flip($irregularPlural);
 
 ?>
